d63aac1b3ff39f24b2a78daea824ac674789ff0d
David Ferreira de Almeida <ferreiradavid361@gmail.com>
2021-05-24 10:54:36 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "David Ferreira de Almeida"
+    return "David Ferreira"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
